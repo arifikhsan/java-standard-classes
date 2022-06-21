148b4f8da259911e2cc0a5c8d8815d25d1e18353
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-06-21 15:15:16 +0700

    math

---
+++ src/main/java/org/example/MathApp.java
@@ -0,0 +1,36 @@
+package org.example;
+
+@SuppressWarnings("unused")
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(1, 2);
+        var max = Math.max(1, 2);
+        var abs = Math.abs(-1);
+        var pow = Math.pow(2, 3);
+        var sqrt = Math.sqrt(4);
+        var ceil = Math.ceil(4.1);
+        var floor = Math.floor(4.1);
+        var round = Math.round(4.1);
+        var random = Math.random();
+        var randomInt = Math.round(Math.random() * 10);
+        var randomDouble = Math.random() * 10;
+        var randomDouble2 = Math.random() * 10 + 1;
+        var pi = Math.PI;
+        var e = Math.E;
+        var tan = Math.tan(Math.PI / 4);
+        var cos = Math.cos(Math.PI / 4);
+        var sin = Math.sin(Math.PI / 4);
+        var tanh = Math.tanh(Math.PI / 4);
+        var cosh = Math.cosh(Math.PI / 4);
+        var sinh = Math.sinh(Math.PI / 4);
+        var exp = Math.exp(Math.PI / 4);
+        var log = Math.log(Math.PI / 4);
+        var log10 = Math.log10(Math.PI / 4);
+        var sqrt2 = Math.sqrt(2);
+        var cbrt = Math.cbrt(8);
+        var atan = Math.atan(Math.PI / 4);
+        var atan2 = Math.atan2(Math.PI / 4, Math.PI / 4);
+        var acos = Math.acos(Math.PI / 4);
+        var asin = Math.asin(Math.PI / 4);
+    }
+}
